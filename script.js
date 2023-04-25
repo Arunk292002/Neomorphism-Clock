08ed597209e6e984ec8dd77ec1aaf821fb37517f
Arunk292002 <111553611+Arunk292002@users.noreply.github.com>
2023-04-25 20:13:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(() =>{
+    
+     let day = new   Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
+
+document.querySelector('#hr').style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+document.querySelector('#mn').style.transform = `rotateZ(${mm}deg)`;
+document.querySelector('#sc').style.transform = `rotateZ(${ss}deg)`;
+    
+})
